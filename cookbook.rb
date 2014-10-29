c0f0dd6b5da347497c07a909d87bfb99438d05bb
Milam <charles.milam@gmail.com>
2014-10-29 00:43:42 -0500

    initial commit
    add initialize method to cookbook class of cookbook app

---
+++ cookbook.rb
@@ -0,0 +1,15 @@
+#We're going to quickly implement a simple Cookbook that can hold Recipes and has 
+#instance variables, initialize methods, attr_accessors and interaction between classes.
+#Milam 102914
+
+class Cookbook
+  class Cookbook
+    def initialize(title)
+      @title = title
+    end
+  end
+  
+  class Recipe
+    
+  end
+end
